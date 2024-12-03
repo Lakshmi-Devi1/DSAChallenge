1aca7501ca311549dfceafddd1357532ad6adaf2
SARIKONDA LAKSHMI DEVI <146585693+Lakshmi-Devi1@users.noreply.github.com>
2024-12-03 21:49:29 +0530

    Create sumofnumbers1.java

---
+++ Math problems/sumofnumbers1.java
@@ -0,0 +1,19 @@
+public class sumofnumbers1{
+    public static void main(String args[]){
+        //sum of first n natural numbers
+        //here they have asked about to know the sum of the first n natral numbers
+        //we can take the n natural numbers where n is the input of teh User 
+        //so that here we can understand that sum of n numbers can be approached through
+        //for loop and with formula
+        //using for loop
+        int sum=0;
+        int n=10;
+        for(int i=0;i<=n;i++){
+            sum+=i;
+            //here for every type of the iteration we have to check the loop and then we will understand
+            //and we iterate the loop until the first n natural number
+        }
+        System.out.println(sum);
+        
+    }
+}
