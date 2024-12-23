bbf04f66b25652400238faa08144a9dab9e9b57b
SARIKONDA LAKSHMI DEVI <146585693+Lakshmi-Devi1@users.noreply.github.com>
2024-12-23 22:38:13 +0530

    Update evenorodd.java

--- Math problems/evenorodd.java
+++ Math problems/evenorodd.java
@@ -1,6 +1,6 @@
 public class evenorodd{
     public static void main(String args[]){
-        //number is even or odd 
+        //number is even or odd
         int num=7;
         if(num%2==0){
             System.out.println("even");
